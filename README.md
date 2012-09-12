f817ee5c758c15271875d9ca615151db0f33d1fa
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2012-09-12 09:06:24 +0200

    Add the tools to test a new installation or an installation update

---
+++ README.md
@@ -0,0 +1,4 @@
+Scripts to test a new installation or an update of an already installation of Silverpeas and
+for each different supported database (MS-SQLServer, Oracle, and PostgreSQL).
+
+Theses scripts require a Bash and Groovy.
---
+++ bin/databasetool/database.groovy
@@ -0,0 +1,57 @@
+import groovy.sql.Sql
+
+def usage =  """\
+Usage: database create|delete RDBMS [HOST]
+       database help
+with:
+  RDBMS the database type: either POSTGRES or MSSQL
+  HOST  the name or the IP address of the server hosting the database
+        (by default localhost).
+Description:
+  create or delete the 'silvertest' database hosting in the specified server."""
+
+if (args.length == 1 && args[0] == "help") {
+  println usage
+  System.exit(0)
+} else if (args.length != 2 && args.length != 3) {
+  println "Missing arguments"
+  println usage
+  System.exit(1)
+}
+
+String action = args[0]
+String rdbms  = args[1]
+String host   = (args.length == 3 ? args[2]:"localhost")
+def sql
+switch(rdbms) {
+  case 'POSTGRES':
+    sql = Sql.newInstance('jdbc:postgresql://' + host + ':5432/',
+      'postgres', 'postgres', 'org.postgresql.Driver')
+    break;
+  case 'MSSQL':
+    sql = Sql.newInstance('jdbc:jtds:sqlserver://' + host + ':1433',
+      'sa', 'wanabe@STAR', 'net.sourceforge.jtds.jdbc.Driver')
+    break;
+  default:
+    println "The RDBMS ${rdbms} isn't currently supported!"
+    System.exit(2)
+}
+
+try {
+  switch(action) {
+    case 'create':
+      sql.execute('create database silvertest')
+      break;
+    case 'delete':
+      sql.execute('drop database silvertest')
+      break;
+    default:
+      println "Unknown action: ${action}"
+      System.exit(4)
+  }
+} catch (Exception ex) {
+  println "Error while executing the action ${action}: ${ex.message}"
+  System.exit(3)
+}
+
+System.exit(0)
